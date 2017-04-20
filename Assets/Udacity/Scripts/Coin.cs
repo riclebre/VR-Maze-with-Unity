72ac31f37995aeed14409be0709c75a8fdb3bb40
Ric Lebre <ric.lebre@gmail.com>
2017-04-20 17:51:53 -0400

    Update on scripts and animations
    Updates on:
    Key.cs
    Door.cs

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
 	//Create a reference to the CoinPoofPrefab
-	public GameObject Poof;
+	public GameObject CoinPoof;
  
 	public void OnCoinClicked() {
 		// Instantiate the CoinPoof Prefab where this coin is located
-		Object.Instantiate(Poof, transform.position, Quaternion.Euler(-90, 0, 0));
+		Object.Instantiate(CoinPoof, transform.position, Quaternion.Euler(-90, 0, 0));
 		/* To make sure the poof animates vertically I used "transform.position, Quaternion.Euler"
 		 more info about it on https://docs.unity3d.com/ScriptReference/Quaternion.Euler.html */
 
--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Door : MonoBehaviour 
 {
-    // Create a boolean value called "locked" that can be checked in OnDoorClicked() 
+    // Create a boolean value called "locked" that can be checked in OnDoorClicked()
+	bool locked = true;
     // Create a boolean value called "opening" that can be checked in Update() 
+	bool opening = false;
 
     void Update() {
-        // If the door is opening and it is not fully raised
-            // Animate the door raising up
+        // If the door is open and it is not fully raised / Animate the door raising up
     }
 
     public void OnDoorClicked() {
         // If the door is clicked and unlocked
             // Set the "opening" boolean to true
+		if (opening == true) {
+			
+		}
         // (optionally) Else
             // Play a sound to indicate the door is locked
     }
@@ -22,5 +26,6 @@ public class Door : MonoBehaviour
     public void Unlock()
     {
         // You'll need to set "locked" to false here
+		locked = false;
     }
 }
